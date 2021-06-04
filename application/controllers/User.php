1f572f747f1e440ced2abda15df1fc06c527df3d
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2021-06-04 16:40:20 +0700

    Last commit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -7,14 +7,14 @@ class User extends CI_Controller {
 	public function get_garages() {
 		$lat = doubleval($this->input->post('lat'));
 		$lng = doubleval($this->input->post('lng'));
-		$garages = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `garages` HAVING distance < 50 ORDER BY distance")->result_array();
+		$garages = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `garages` ORDER BY distance")->result_array();
 		echo json_encode($garages);
 	}
 	
 	public function get_oprekers() {
 		$lat = doubleval($this->input->post('lat'));
 		$lng = doubleval($this->input->post('lng'));
-		$oprekers = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `oprekers` HAVING distance < 50 ORDER BY distance")->result_array();
+		$oprekers = $this->db->query("SELECT *, SQRT(POW(69.1 * (latitude - " . $lat . "), 2) + POW(69.1 * (" . $lng . " - longitude) * COS(latitude / 57.3), 2)) AS distance FROM `oprekers` ORDER BY distance")->result_array();
 		echo json_encode($oprekers);
 	}
 	
